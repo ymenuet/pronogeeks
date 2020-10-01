1c221668c48928ab64a070b5a714f188e4f93dbe
Yvan Menuet <yvan.menuet@gmail.com>
2020-09-30 20:14:35 -0500

    offset correction on GeekLeagues page

--- src/pages/GeekLeagues.js
+++ src/pages/GeekLeagues.js
@@ -25,7 +25,7 @@ const GeekLeagues = ({ loading }) => {
                 <div className='container'>
                     <h2>Mes Ligues Geek</h2>
                     <div className='my-geekleagues row'>
-                        {geekLeagues.map(geekLeague => <div key={geekLeague._id} className='col-10 offset-1 col-lg-6 offset-lg-0 geekleague-card-container'>
+                        {geekLeagues.map(geekLeague => <div key={geekLeague._id} className='col-10 col-lg-6 geekleague-card-container'>
                             <div className='geekleague-card'>
                                 <h4>{geekLeague.name}</h4>
                                 <h6>Créée par {geekLeague.creator.username} en {new Date(geekLeague.createdAt).getMonth().length > 1 ? new Date(geekLeague.createdAt).getMonth() : `0${new Date(geekLeague.createdAt).getMonth()}`}/{new Date(geekLeague.createdAt).getFullYear()}</h6>
@@ -37,8 +37,7 @@ const GeekLeagues = ({ loading }) => {
                     <Link to='/myGeekLeagues/new' className='btn my-btn new-league geekleagues-page-btn'>Créer une ligue</Link>
                 </div>
             )}
-
-    </div >
+    </div>
 }
 
 export default GeekLeagues
