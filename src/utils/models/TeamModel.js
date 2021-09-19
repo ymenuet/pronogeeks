c92bf77de4bdf83ead446f7ea008852c61f54b3a
Yvan Menuet <yvan.menuet@wizeline.com>
2021-09-19 00:04:06 -0500

    team model done

--- src/utils/models/TeamModel.js
+++ src/utils/models/TeamModel.js
@@ -1,3 +1,19 @@
 import PropTypes from 'prop-types';
 
-export default PropTypes.shape({});
+export default PropTypes.shape({
+  name: PropTypes.string,
+  code: PropTypes.string,
+  stadium: PropTypes.string,
+  country: PropTypes.string,
+  logo: PropTypes.string,
+  season: PropTypes.string,
+  rank: PropTypes.number,
+  points: PropTypes.number,
+  goalsDiff: PropTypes.number,
+  matchsPlayed: PropTypes.number,
+  win: PropTypes.number,
+  draw: PropTypes.number,
+  lose: PropTypes.number,
+  goalsFor: PropTypes.number,
+  goalsAgainst: PropTypes.number,
+});
